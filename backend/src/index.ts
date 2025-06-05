3214632282a5778afd0c78325a6da507216bb4aa
abhradeepbarman <abhradeep709@gmail.com>
2025-06-05 21:31:46 +0530

    basic backend setup done

---
+++ backend/src/index.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import { config } from "./config";
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+app.listen(config.PORT, () => {
+    console.log(`Server running on port ${config.PORT}`);
+});
+
+export default app;
